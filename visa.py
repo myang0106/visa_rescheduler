21af7abf3fc05a580d97e3d5a8329b1545a26bff
Mo Yang <mengsl1023@gmail.com>
2022-09-12 17:06:47 -0700

    established initial framework. loggin ready

--- visa.py
+++ visa.py
@@ -21,6 +21,7 @@ from sendgrid.helpers.mail import Mail
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+print(config.sections())
 
 USERNAME = config['USVISA']['USERNAME']
 PASSWORD = config['USVISA']['PASSWORD']
@@ -36,7 +37,7 @@ PUSH_USER = config['PUSHOVER']['PUSH_USER']
 LOCAL_USE = config['CHROMEDRIVER'].getboolean('LOCAL_USE')
 HUB_ADDRESS = config['CHROMEDRIVER']['HUB_ADDRESS']
 
-REGEX_CONTINUE = "//a[contains(text(),'Continuar')]"
+REGEX_CONTINUE = "//a[contains(text(),'Continue')]"
 
 
 # def MY_CONDITION(month, day): return int(month) == 11 and int(day) >= 5
